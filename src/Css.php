5512c908dcc18a32376b6766b46a784cf541aa89
Derek Stephen McLean <delboy1978uk@gmail.com>
2015-01-05 13:30:51 +0000

    added type="text/css"

--- src/Css.php
+++ src/Css.php
@@ -6,7 +6,7 @@ class Css
 {
     public static function add()
     {
-        $style = '<style>';
+        $style = '<style type="text/css">';
         $style .= file_get_contents(__DIR__.'/karate.css');
         $style .= '</style>';
         return $style;
